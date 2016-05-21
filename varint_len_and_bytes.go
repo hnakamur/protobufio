af234fc0ad08425379df417e9a02028a33ef8835
Hiroaki Nakamura <hnakamur@gmail.com>
2016-05-22 02:22:07 +0900

    Embed VarintReader in BytesWriter

--- varint_len_and_bytes.go
+++ varint_len_and_bytes.go
@@ -5,19 +5,19 @@ import "io"
 // BytesWriter writes a length in the variable-length encoding
 // and the following bytes.
 type BytesWriter struct {
-	io.Writer
+	*VarintWriter
 }
 
 // NewBytesWriter creates a BytesWriter with the undelying writer.
 func NewBytesWriter(w io.Writer) *BytesWriter {
-	return &BytesWriter{Writer: w}
+	return &BytesWriter{VarintWriter: NewVarintWriter(w)}
 }
 
 // WriteVarintLenAndBytes writes a length in the variable-length encoding
 // and the following bytes in buf to the underlying writer. It returns the number of
 // bytes written for the length and the number of bytes written for bytes in buf.
 func (w *BytesWriter) WriteVarintLenAndBytes(buf []byte) (n1, n2 int, err error) {
-	n1, err = NewVarintWriter(w).WriteVarint(int64(len(buf)))
+	n1, err = w.WriteVarint(int64(len(buf)))
 	if err != nil {
 		return
 	}
@@ -28,12 +28,12 @@ func (w *BytesWriter) WriteVarintLenAndBytes(buf []byte) (n1, n2 int, err error)
 // BytesReader reads a length in the variable-length encoding
 // and the following bytes.
 type BytesReader struct {
-	io.Reader
+	*VarintReader
 }
 
 // NewBytesReader creates a BytesReader with the underlying reader.
 func NewBytesReader(r io.Reader) *BytesReader {
-	return &BytesReader{Reader: r}
+	return &BytesReader{VarintReader: NewVarintReader(r)}
 }
 
 // ReadVarintLenAndBytes reads a length in the variable-length encoding
@@ -42,7 +42,7 @@ func NewBytesReader(r io.Reader) *BytesReader {
 // read for the length and the number of bytes read for the following
 // bytes. You can get the following bytes with bufOrNewBuf[:n2].
 func (r *BytesReader) ReadVarintLenAndBytes(buf []byte) (bufOrNewBuf []byte, n1, n2 int, err error) {
-	length, n1, err := NewVarintReader(r).ReadVarint()
+	length, n1, err := r.ReadVarint()
 	if err != nil {
 		return
 	}
