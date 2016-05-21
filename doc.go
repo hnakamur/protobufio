ec6834c31b6e9e1461763dab71de1a5f3fef4bdf
Hiroaki Nakamura <hnakamur@gmail.com>
2016-05-22 02:24:39 +0900

    Add a link to the spec of the variable-length encoding

--- doc.go
+++ doc.go
@@ -1,4 +1,6 @@
 // Package protobufio provides a reader and a writer for reading and writing
 // protocol buffers prefixed with the length in the variable-length
 // encoding.
+// See https://developers.google.com/protocol-buffers/docs/encoding#simple
+// for the specification of variable-length encoding.
 package protobufio
